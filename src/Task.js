f7d540819c59b464cf2e7cf8d3405950bf89552b
Hazal Taştan <hazl.dogann@gmail.com>
2023-10-17 16:58:45 +0300

    date-fns complete

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,36 @@
+import { differenceInDays, formatDistanceToNow } from "date-fns";
+import { tr } from "date-fns/locale";
 import React from "react";
 
 const Task = ({ taskObj, onComplete }) => {
-
+  const deadlineInDate = new Date(taskObj.deadline);
+  const deadlineInText = formatDistanceToNow(deadlineInDate, {
+    addSuffix: true,
+    locale: tr,
+  });
+  const today = new Date();
+  function getClassName(date) {
+    const diff = differenceInDays(date, today);
+    return diff > 3 ? "normal" : "urgent";
+  }
+  const bg = getClassName(deadlineInDate);
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">
+        son teslim: <span className={bg}>{deadlineInText}</span>
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+      )}
     </div>
   );
 };
